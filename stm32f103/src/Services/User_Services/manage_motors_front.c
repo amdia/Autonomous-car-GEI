2d2b696dc1d533d677d75290462399f4083d85b0
marcyrk <marcy.rakotopare@gmail.com>
2016-11-22 09:15:23 +0100

    delete file

--- stm32f103/src/Services/User_Services/manage_motors_front.c
+++
@@ -1,9 +0,0 @@
-#include "manage_motors.h"
-
-void motor_front_stop(Hall_Position pos){
-	if(pos == HALL_AVG || pos == HALL_AVD){
-		disableFrontMotor();
-		setFrontDirection((Direction)front);
-		front = STOP;
-	}
-}
